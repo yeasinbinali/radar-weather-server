14cdd911eb940fa85fc2082079e1fc61419f3b59
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-15 12:01:00 +0600

    get and post api added for getting favourite list

--- index.js
+++ index.js
@@ -14,31 +14,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        const favouriteListCollection = client.db('radar-server').collection('favouriteList');
+
+        app.get('/favouriteList', async (req, res) => {
+            const result = await favouriteListCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/favouriteList', async (req, res) => {
+            const favouriteOne = req.body;
+            const result = await favouriteListCollection.insertOne(favouriteOne);
+            res.send(result);
+        })
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Radar server is running')
+    res.send('Radar server is running')
 })
 
 app.listen(port, () => {
-  console.log(`Radar server running on port ${port}`)
+    console.log(`Radar server running on port ${port}`)
 })
