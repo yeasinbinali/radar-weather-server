dba8149f04dd4531193d68c02938fe96e907085d
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-16 14:36:03 +0600

    cors updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
     origin: [
-        "http://localhost:5173",
+        // "http://localhost:5173",
         "https://radar-weather.netlify.app"
     ]
 }));
